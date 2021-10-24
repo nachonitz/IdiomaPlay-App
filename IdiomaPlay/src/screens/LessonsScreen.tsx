9f485cf05376cae0ab081497e02f03ad124eeb40
Lucas <lucas.rialb@creativecoefficient.net>
2021-10-24 16:46:44 -0300

    Green checkmark

--- IdiomaPlay/src/screens/LessonsScreen.tsx
+++ IdiomaPlay/src/screens/LessonsScreen.tsx
@@ -1,73 +1,79 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text, StyleSheet, Touchable, TouchableOpacity } from 'react-native';
-import { CustomHeaderScreen } from '../components/CustomHeaderScreen'
-import { styles } from '../theme/appTheme'
-import { Card } from 'react-native-elements'
-import { ParamListBase, useNavigation } from '@react-navigation/core';
-import { Screens } from '../navigator/Screens';
-import { StackNavigationProp } from '@react-navigation/stack'
-import IdiomaPlayApi from '../api/IdiomaPlayApi';
-import { colors } from '../theme/colors';
-import { config } from '../../Configuration';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Touchable,
+  TouchableOpacity,
+} from "react-native";
+import { CustomHeaderScreen } from "../components/CustomHeaderScreen";
+import { styles } from "../theme/appTheme";
+import { Card } from "react-native-elements";
+import { ParamListBase, useNavigation } from "@react-navigation/core";
+import { Screens } from "../navigator/Screens";
+import { StackNavigationProp } from "@react-navigation/stack";
+import IdiomaPlayApi from "../api/IdiomaPlayApi";
+import { colors } from "../theme/colors";
+import { config } from "../../Configuration";
+import Icon from "react-native-vector-icons/Ionicons";
 
-
-export const LessonsScreen = ({route}:any) => {
-  const navigation = useNavigation<StackNavigationProp<ParamListBase>>()
+export const LessonsScreen = ({ route }: any) => {
+  const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
   const [lessons, setLessons] = useState([]);
   const [examId, setExamId] = useState(-1);
-  const [completedLessons, setcompletedLessons] = useState <Array<any>>([])
-  const [completedExam, setCompletedExam] = useState(false)
+  const [completedLessons, setcompletedLessons] = useState<Array<any>>([]);
+  const [completedExam, setCompletedExam] = useState(false);
 
   const getLessons = async () => {
     try {
-      console.log(route.params.unitId)
-      const resp = await IdiomaPlayApi.get('/units/' + route.params.unitId)
-      setExamId(resp.data.exam.id)
-      setLessons(resp.data.lessons)
-      console.log(resp.data.lessons)
-      const completed: Array<any> = []
-      const length = resp.data.lessons.length
+      console.log(route.params.unitId);
+      const resp = await IdiomaPlayApi.get("/units/" + route.params.unitId);
+      setExamId(resp.data.exam.id);
+      setLessons(resp.data.lessons);
+      console.log(resp.data.lessons);
+      const completed: Array<any> = [];
+      const length = resp.data.lessons.length;
       const lessons = resp.data.lessons;
-      for(var i = 0; i < length; i++){
+      for (var i = 0; i < length; i++) {
         const lessonId = lessons[i].id;
-        const dict = {"lessonId":lessonId, "value":false}
-        completed.push(dict)
+        const dict = { lessonId: lessonId, value: false };
+        completed.push(dict);
       }
       try {
-        const participationsResp = await IdiomaPlayApi.get('participations',
-        {
+        const participationsResp = await IdiomaPlayApi.get("participations", {
           params: {
-            'limit': 500,
-            'page': 1,
-            'user': 1,
-            'unit': route.params.unitId
-          }
-        }
-        )
-        console.log(participationsResp)
-        const length = participationsResp.data.items.length
+            limit: 500,
+            page: 1,
+            user: 1,
+            unit: route.params.unitId,
+          },
+        });
+        console.log(participationsResp);
+        const length = participationsResp.data.items.length;
         const participations = participationsResp.data.items;
-        console.log(participations)
-        for (var i = 0; i < length; i++){
-          const exam = participations[i].exam
+        console.log(participations);
+        for (var i = 0; i < length; i++) {
+          const exam = participations[i].exam;
           if (exam) {
-            const examId = participations[i].exam.id
-            const correctExercises = participations[i].correctExercises
+            const examId = participations[i].exam.id;
+            const correctExercises = participations[i].correctExercises;
             if (correctExercises >= config.passingAmountOfExcercisesPerExam)
-            setCompletedExam(true)
-            continue
+              setCompletedExam(true);
+            continue;
           }
-          const lessonId = participations[i].lesson.id
-          const correctExercises = participations[i].correctExercises
+          const lessonId = participations[i].lesson.id;
+          const correctExercises = participations[i].correctExercises;
           if (correctExercises >= config.passingAmountOfExcercisesPerLesson) {
-            const index = completed.findIndex((element) => element.lessonId === lessonId)
-            completed[index] = {"lessonId":lessonId, "value":true}
+            const index = completed.findIndex(
+              (element) => element.lessonId === lessonId
+            );
+            completed[index] = { lessonId: lessonId, value: true };
           }
         }
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
-      setcompletedLessons(completed)
+      setcompletedLessons(completed);
     } catch (error) {
       // setError(true);
       console.error(error);
@@ -79,62 +85,96 @@ export const LessonsScreen = ({route}:any) => {
   }, []);
 
   return (
-    <CustomHeaderScreen logo  profile>
+    <CustomHeaderScreen logo profile>
       <View style={homeStyles.container}>
-
-      {lessons.length > 0 && completedLessons.length > 0 && lessons.map((lesson:any, index) => (
-        <TouchableOpacity
-          onPress={() => {
-            navigation.replace(
-              Screens.exercises, 
-              { lessonId: lesson['id'],
-                unitId: route.params.unitId
-              }
-            )}}
-          activeOpacity={0.8}
-          disabled={completedLessons[index].value}
-          key={lesson.id}
-        >
-          <Card containerStyle={[homeStyles.card, completedLessons[index].value && {backgroundColor: colors.correct}]}>
-            <Text style={homeStyles.cardTitle}>{lesson.title}</Text>
-          </Card>
-        </TouchableOpacity>
-      ))}
-        {lessons.length > 0 && completedLessons.length > 0 &&
-        <TouchableOpacity
-          onPress={() => {
-            navigation.replace(
-              Screens.exercises, 
-              { lessonId: 1,
+        {lessons.length > 0 &&
+          completedLessons.length > 0 &&
+          lessons.map((lesson: any, index) => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.replace(Screens.exercises, {
+                  lessonId: lesson["id"],
+                  unitId: route.params.unitId,
+                });
+              }}
+              activeOpacity={0.8}
+              disabled={completedLessons[index].value}
+              key={lesson.id}
+            >
+              <Card containerStyle={[homeStyles.card]}>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    // Fix harcoded width
+                    width: 300,
+                  }}
+                >
+                  <Text style={homeStyles.cardTitle}>{lesson.title}</Text>
+                  {completedLessons[index].value && (
+                    <Icon
+                      name="checkmark-outline"
+                      size={30}
+                      color={colors.correct}
+                    />
+                  )}
+                </View>
+              </Card>
+            </TouchableOpacity>
+          ))}
+        {lessons.length > 0 && completedLessons.length > 0 && (
+          <TouchableOpacity
+            onPress={() => {
+              navigation.replace(Screens.exercises, {
+                lessonId: 1,
                 examId: examId,
                 isExam: true,
-                unitId: route.params.unitId
-              }
-            )}}
-          activeOpacity={0.8}
-          disabled={completedLessons.findIndex((element) => element.value === false) !== -1 || completedExam}
-        >
-          <Card containerStyle={[homeStyles.card, {backgroundColor: completedExam?colors.correct: completedLessons.findIndex((element) => element.value === false) !== -1?'lightgrey' : colors.lightPrimary}]}>
-            <Text style={homeStyles.cardTitle}>Test</Text>
-          </Card>
-        </TouchableOpacity>}
-      
-      <View style={homeStyles.spacer}/>
+                unitId: route.params.unitId,
+              });
+            }}
+            activeOpacity={0.8}
+            disabled={
+              completedLessons.findIndex(
+                (element) => element.value === false
+              ) !== -1 || completedExam
+            }
+          >
+            <Card
+              containerStyle={[
+                homeStyles.card,
+                {
+                  backgroundColor: completedExam
+                    ? colors.correct
+                    : completedLessons.findIndex(
+                        (element) => element.value === false
+                      ) !== -1
+                    ? "lightgrey"
+                    : colors.lightPrimary,
+                },
+              ]}
+            >
+              <Text style={homeStyles.cardTitle}>Test</Text>
+            </Card>
+          </TouchableOpacity>
+        )}
+
+        <View style={homeStyles.spacer} />
       </View>
     </CustomHeaderScreen>
-  )
-}
+  );
+};
 
 const homeStyles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 20
+    marginTop: 20,
   },
   card: {
     height: 75,
     borderRadius: 10,
     borderWidth: 0.5,
-    borderColor: 'lightgrey',
+    borderColor: "lightgrey",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -144,24 +184,24 @@ const homeStyles = StyleSheet.create({
     shadowRadius: 5.4,
     elevation: 9,
     marginBottom: 15,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "flex-start",
   },
   cardTitle: {
     fontSize: 18,
     color: colors.darkPrimary,
-    fontWeight: 'bold',
-    textAlign: 'center'
+    fontWeight: "bold",
+    textAlign: "left",
   },
   cardDescription: {
     fontSize: 17,
-    marginBottom: 15
+    marginBottom: 15,
   },
   cardSubtitle: {
     fontSize: 15,
-    color: 'grey'
-  }, 
+    color: "grey",
+  },
   spacer: {
-    height: 100
-  }
-})
+    height: 100,
+  },
+});
