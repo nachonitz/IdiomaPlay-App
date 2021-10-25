67b82a40d0932c3d942a3e39a36255bb9b6f40cf
Lucas <lucas.rialb@creativecoefficient.net>
2021-10-25 18:06:18 -0300

    Listening hint

--- IdiomaPlay/src/services/Dictionary.tsx
+++ IdiomaPlay/src/services/Dictionary.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export const TypeToInstruction = (type: string) => {
-  
-    if(type.localeCompare('complete') === 0)
-      return 'Completa la frase con la opción correcta'
-      
-    if(type.localeCompare('translate_old_to_new') === 0)
-      return 'Traduzca la frase'
-      
-    if(type.localeCompare('translate_new_to_old') === 0)
-      return 'Traduzca la frase'
+  if (type.localeCompare("complete") === 0)
+    return "Completa la frase con la opción correcta";
 
-  return type
-}
+  if (type.localeCompare("translate_old_to_new") === 0)
+    return "Traduzca la frase";
+
+  if (type.localeCompare("translate_new_to_old") === 0)
+    return "Traduzca la frase";
+
+  if (type.localeCompare("listen") === 0)
+    return "Escuhe y seleccione la opción correcta";
+
+  return type;
+};
